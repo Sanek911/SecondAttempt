9123c1b69c40b329053100fa10bb194b83188e89
dmitryderyabin <d.deryabin@synergysportstech.com>
2017-02-20 11:53:37 +0200

    1

---
+++ SecondAttempt/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+
+namespace SecondAttempt
+{
+    /// <summary>
+    ///     Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int _count;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            RadListBox.Items.Add($"Count is: {_count++}");
+        }
+    }
+}
