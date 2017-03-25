7169c08e21f0a94856aae6ec1726dd06f9bddf29
a.pitsyk <1@1.com>
2017-03-25 19:33:35 +0200

    .

--- SecondAttempt/App.xaml.cs
+++ SecondAttempt/App.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -13,5 +14,6 @@ namespace SecondAttempt
     /// </summary>
     public partial class App : Application
     {
+
     }
 }
---
+++ SecondAttempt/Extensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace SecondAttempt
+{
+    public static class Extensions
+    {
+        #region Methods
+
+        public static string GetPropertyName<T>(Expression<Func<T>> propertyDelegate)
+        {
+            var expression = (MemberExpression)propertyDelegate.Body;
+            return expression.Member.Name;
+        }
+
+        #endregion
+    }
+}
